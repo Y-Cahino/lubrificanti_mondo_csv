a6d867321f7135f4bf795b38f12b4444d1eb3eec
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-05-05 13:03:12 +0200

    Aggiungere i file di progetto.

---
+++ elaborazione_csv/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace elaborazione_csv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.op = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // op
+            // 
+            this.op.Location = new System.Drawing.Point(63, 93);
+            this.op.Name = "op";
+            this.op.Size = new System.Drawing.Size(75, 23);
+            this.op.TabIndex = 0;
+            this.op.Text = "Apri";
+            this.op.UseVisualStyleBackColor = true;
+            this.op.Click += new System.EventHandler(this.op_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(460, 18);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(309, 420);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.op);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button op;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ elaborazione_csv/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace elaborazione_csv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void op_Click(object sender, EventArgs e)
+        {
+            apri();
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        #region funzioni
+        public void apri()
+        {
+            var reader = new StreamReader(File.OpenRead(@"Cahino.csv"));
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+                var line2 = reader.ReadLine();
+                var values2 = line.Split(' ');
+
+                listView1.Items.Add(values[0],values2[0]);
+                listView1.Items.Add(values[1],values2[1]);
+                foreach (string coloumn1 in listView1.Items) //per ogni valore presente sulla listview
+                {
+                    listView1.Columns.Add(coloumn1,20); //permette di creare delle colonne comprendenti le info sul file
+                }
+                foreach (string coloumn2 in listView1.Items)
+                {
+                    listView1.Columns.Add(coloumn2, 20);
+                }
+
+
+            }
+            #endregion
+
+
+        }
+    }
+}
