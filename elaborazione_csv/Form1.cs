18d4eed466aa6c4c82c07006063f01ec0fe6d047
Cahinosilva.20184 <Cahinosilva.20184@LABI2-13PC14>
2023-05-24 13:59:54 +0200

    funziione

--- elaborazione_csv/Form1.cs
+++ elaborazione_csv/Form1.cs
@@ -13,9 +13,18 @@ namespace elaborazione_csv
 {
     public partial class Form1 : Form
     {
+        public struct lubs
+        {
+            public string anno, nazione, info, peso, note;
+        }
+        public lubs[] p;
+        public lubs p2;
+        public int d;
         public Form1()
         {
             InitializeComponent();
+            lubs[] p = new lubs[d];
+            d = 0;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -25,6 +34,8 @@ namespace elaborazione_csv
         private void op_Click(object sender, EventArgs e)
         {
             apri();
+            st(p2);
+            d++;
         }
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -38,11 +49,7 @@ namespace elaborazione_csv
             {
                 var line = reader.ReadLine();
                 var values = line.Split(';');
-                var line2 = reader.ReadLine();
-                var values2 = line.Split(' ');
-
-                listView1.Items.Add(values[0],values2[0]);
-                listView1.Items.Add(values[1],values2[1]);
+                listView1.Items.Add(values[d]);
                 foreach (string coloumn1 in listView1.Items) //per ogni valore presente sulla listview
                 {
                     listView1.Columns.Add(coloumn1,20); //permette di creare delle colonne comprendenti le info sul file
@@ -51,12 +58,16 @@ namespace elaborazione_csv
                 {
                     listView1.Columns.Add(coloumn2, 20);
                 }
+                
 
-
+                
             }
-            #endregion
-
-
         }
+        public string st(lubs p)
+        {
+
+            return p2.anno;
+        }
+        #endregion
     }
 }
